6092c60623e4bef887205e6419ed917b7b6ccf03
Michael Brunner <71121348+Brunner246@users.noreply.github.com>
2022-07-12 09:49:02 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const nodemailer = require('nodemailer');
+
+try {
+  const transporter = nodemailer.createTransport({
+    host: core.getInput('server_address'),
+    port: core.getInput('port'),
+    auth: {
+      user: core.getInput('username'),
+      pass: core.getInput('password')
+    }
+  });
+
+  const options = {
+    from: core.getInput('from'),
+    to: core.getInput('to'),
+    subject: core.getInput('subject'),
+    text: core.getInput('body')
+  };
+
+  transporter.sendMail(options, (error, info) => {
+    if (error) {
+      throw new Error('Error sending email:' + error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
